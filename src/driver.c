7e8d511001c7b99f6d972856f969bd0c05dec3fa
clayton williams <clayton.e.williams15.mil@mail.mil>
2023-08-21 09:58:43 -0400

    insert function working when divergence is at the last character of root word.

--- src/driver.c
+++ src/driver.c
@@ -1,115 +1,26 @@
-#include <errno.h>
-#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "errno.h"
 
 #include "../include/radix.h"
 
-#define MAX_CHAR 256
+int main (void) {
+        trie_t *root = radix_create();
 
-int main(int argc, char *argv[])
-{
-    // Return value
-    int result = 0;
-
-    // Expects two arguments, file to read for adding, file to read for search
-    // WARNING: Cannot be same file (double fopen)
-    //
-    if (argc != 3) {
-        fprintf(stderr, "Expected %d args, got %d\n", 2, argc - 1);
-        goto SHUTDOWN;
-    }
-
-    FILE *fp_read = fopen(argv[argc - 2], "r"); // argv[1]
-    if (!fp_read) {
-        perror("[ERR!] Read File");
-        errno = 0;
-        result = 1;
-        goto SHUTDOWN;
-    }
-
-    trie_t *trie = radix_create();
-    if (!trie) {
-        fprintf(stderr, "Trie alloc() failure\n");
-        result = 1;
-        goto FCLOSE_READ;
-    }
-
-    char *lineptr = calloc(MAX_CHAR, sizeof(*lineptr));
-    if (!lineptr) {
-        perror("String allocation");
-	errno = 0;
-        result = 1;
-        goto CLEANUP_TRIE;
-    }
-
-    const char *accept = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcedfghijklmnopqrstuvwxyz";
-    uint64_t str_len = 0;
-
-    // Adds one word at a time to the trie
-    // NOTE: Assumes one word per line, else strspn will reject line
-    //
-    while (fgets(lineptr, MAX_CHAR, fp_read)) {
-        // Cleans line of newline, if it exists
-        //
-        str_len = strlen(lineptr);
-        if ('\n' == lineptr[str_len - 1]) {
-            lineptr[--str_len] = '\0';
+        FILE *fp = fopen("./data.txt", "r");
+        if (!fp) {
+                perror("fopen");
+                errno = 0;
+                return;
         }
-
-        // Ignores any string to upper AND lowercase
-        // On student to determine how to handle within insert function
-        // - Opt 1: Reject entirely
-        // - Opt 2: Squash to lowercase/uppercase
-        // - Opt 3: Handle Upper and lower values uniquely (not recommended)
-        //
-        // Determined usage must be included in test plan
-        //
-        if (str_len == strspn(lineptr, accept)) {
-            radix_insert_word(trie, lineptr);
+        int nl_loc = 0;
+        char *string = calloc(50, sizeof(char));
+        while(fgets(string, 50, fp)) {
+                nl_loc = strcspn(string, "\n");
+                string[nl_loc] = '\0';
+                radix_insert_word(root, string);
         }
-    }
+        radix_print(root);
 
-    FILE *fp_search = fopen(argv[argc - 1], "r"); // argv[2]
-    if (!fp_search) {
-        perror("[ERR!] Search File");
-        errno = 0;
-        result = 1;
-        goto CLEANUP_PTR;
-    }
-
-    // Searches trie for one word at a time
-    // NOTE: Assumes one word per line, else strspn will reject line
-    //
-    while (fgets(lineptr, MAX_CHAR, fp_search)) {
-        // Cleans line of newline, if it exists
-        //
-        str_len = strlen(lineptr);
-        if ('\n' == lineptr[str_len - 1]) {
-            lineptr[--str_len] = '\0';
-        }
-
-        // Allows upper/lower/combination words through
-        // Students choice on how to handle within find_word() 
-        // but should logically match the insertion scenario allowed
-        //
-        // Determined usage must be included in test plan
-        //
-        if (str_len == strspn(lineptr, accept)) {
-            if (radix_find_word(trie, lineptr)) {
-                printf("EXISTS : %s\n", lineptr);
-            }
-        }
-    }
-
-    fclose(fp_search);
-CLEANUP_PTR:
-    free(lineptr);
-CLEANUP_TRIE:
-    radix_delete(&trie);
-FCLOSE_READ:
-    fclose(fp_read);
-SHUTDOWN:
-    return result;
 }
--- src/radix.c
+++ src/radix.c
@@ -1,12 +1,8 @@
-/** @file radix.c
- *
- * @brief Radix Trie library for word lookup
- *
-*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-
+#include <string.h>
+#include <errno.h>
 
 #include "../include/radix.h"
 
@@ -21,6 +17,11 @@ struct trie_t {
         bool b_is_word;
 };
 
+static trie_t *radix_insert_rec(trie_t *node, char *word, int len, int index);
+static trie_t *radix_create_node(int len);
+static int get_prefix_index(const char *word, const char *new_word); 
+
+
 trie_t *radix_create(void)
 {
         return calloc(1, sizeof(trie_t));
@@ -28,8 +29,51 @@ trie_t *radix_create(void)
 
 int radix_insert_word(trie_t *trie, const char *word)
 {
-    ;
+        if ((!trie) || (!word)) {
+                fprintf(stderr, "radix_insert_word: Invalid argument - NULL\n");
+                return 0;
+        }
+
+        trie_t *tmp = trie;
+        int len = strlen(word);
+        char *word_cpy = calloc(len + 1, sizeof(char));
+        strncpy(word_cpy, word, len);
+        int index = CHAR_TO_INDEX(word[0]);
+        radix_insert_rec(tmp, word_cpy, len, index);
 }
+
+static trie_t *radix_insert_rec(trie_t *node, char *word, int len, int index) {
+        if (!node->children[index]) {
+                node->children[index] = radix_create_node(len);
+                memcpy(node->children[index]->word, word, len);
+                node->children[index]->b_is_word = true;
+                return node->children[index];
+        }
+        
+        trie_t *tmp = node->children[index];
+        int root_word_len = strlen(tmp->word);
+        int prefix_idx = get_prefix_index(word, tmp->word);
+        printf("With word %s, prefix idx is %d\n", word, prefix_idx);
+        printf("len is %d and rootlen is %d\n", len, root_word_len);
+        char *word_cpy = calloc(root_word_len, sizeof(char));
+        word += prefix_idx;
+        if (prefix_idx == root_word_len) {
+                printf("True!\n");
+                int index = CHAR_TO_INDEX(word[0]);
+                return radix_insert_rec(tmp, word, (len - prefix_idx), index);
+        }
+}
+
+static trie_t *radix_create_node(int len) {
+        trie_t *node = calloc(1, sizeof(*node));
+        if (!node) {
+                perror("calloc");
+                errno = 0;
+        }
+        node->word = calloc((len + 1), sizeof(char));
+        return node;
+}
+
 
 int radix_remove_word(trie_t *trie, const char *word)
 {
@@ -51,11 +95,23 @@ void radix_delete(trie_t **trie)
     ;
 }
 
+void radix_print(trie_t *root) {
+        printf("%s\n", root->children[15]->word);
+        printf("%s\n", root->children[15]->children[18]->word);
+}
+
+static int get_prefix_index(const char *word, const char *new_word) {
+        int index = 0;
+        printf("comparing %s and %s\n", word, new_word);
+        while (word[index] == new_word[index]) {
+                ++index;
+        }
+        return index;
+}
+
 /*
 void radix_print_nodes(trie_t *trie)
 {
     ;
 }
- */
-
-/*** end of file ***/
+*/
