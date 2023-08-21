dbd49cb4deeeba07a54fb60282f0bb08eeb61700
clayton williams <clayton.e.williams15.mil@mail.mil>
2023-08-21 14:47:41 -0400

    Insert working fully with p_dict.txt. Needs some cleanup and reorganization.

--- src/driver.c
+++ src/driver.c
@@ -8,8 +8,8 @@
 int main (void) {
         trie_t *root = radix_create();
 
-        // FILE *fp = fopen("./test/p_dict.txt", "r");
-        FILE *fp = fopen("./data.txt", "r");
+        FILE *fp = fopen("./test/p_dict.txt", "r");
+        // FILE *fp = fopen("./data.txt", "r");
         if (!fp) {
                 perror("fopen");
                 errno = 0;
