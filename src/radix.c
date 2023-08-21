e2a69a7ee75d2791b6c8f6c19e56b213afb556d4
clayton williams <clayton.e.williams15.mil@mail.mil>
2023-08-21 08:25:08 -0400

    functionality aded for creating trie.

--- src/radix.c
+++ src/radix.c
@@ -3,16 +3,27 @@
  * @brief Radix Trie library for word lookup
  *
 */
+#include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-#include "radix.h"
+
+#include "../include/radix.h"
+
+#define CHAR_TO_INDEX(c) ((int)c - (int)'a')
+#define INDEX_TO_CHAR(i) ((char)i + (char)'a')
+
+#define NUM_CHARS 26
 
 struct trie_t {
+        struct trie_t *children[NUM_CHARS];
+        bool b_is_word;
+        char *word;
 };
 
 trie_t *radix_create(void)
 {
-    ;
+        return calloc(1, sizeof(trie_t));
 }
 
 int radix_insert_word(trie_t *trie, const char *word)
@@ -27,7 +38,7 @@ int radix_remove_word(trie_t *trie, const char *word)
 
 int radix_find_word(trie_t *trie, const char *target)
 {
-    ;
+    ;   
 }
 
 int radix_find_prefix(trie_t *trie, const char *prefix)
