2317587bd6e0626aab54f1765f851a07f7c1ded8
clayton williams <clayton.e.williams15.mil@mail.mil>
2023-08-21 08:13:37 -0400

    Repo initialized.

---
+++ src/radix.c
@@ -0,0 +1,50 @@
+/** @file radix.c
+ *
+ * @brief Radix Trie library for word lookup
+ *
+*/
+#include <stdlib.h>
+
+#include "radix.h"
+
+struct trie_t {
+};
+
+trie_t *radix_create(void)
+{
+    ;
+}
+
+int radix_insert_word(trie_t *trie, const char *word)
+{
+    ;
+}
+
+int radix_remove_word(trie_t *trie, const char *word)
+{
+    ;
+}
+
+int radix_find_word(trie_t *trie, const char *target)
+{
+    ;
+}
+
+int radix_find_prefix(trie_t *trie, const char *prefix)
+{
+    ;
+}
+
+void radix_delete(trie_t **trie)
+{
+    ;
+}
+
+/*
+void radix_print_nodes(trie_t *trie)
+{
+    ;
+}
+ */
+
+/*** end of file ***/
