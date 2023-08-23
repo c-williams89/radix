a2bc10ccd48599975e28108b2d769487bfc0cbb9
clayton williams <clayton.e.williams15.mil@mail.mil>
2023-08-23 08:31:31 -0400

    radix_find_prefix functionality fixed. Able to print if the prefix is a valid node or partial node.

--- src/driver.c
+++ src/driver.c
@@ -18,40 +18,39 @@ int main(void)
 	}
 	// int nl_loc = 0;
 	// char *string = calloc(50, sizeof(char));
-        // char string[5] = { '\0 '};
+	// char string[5] = { '\0 '};
 	// while (fgets(string, 50, fp)) {
-	// 	nl_loc = strcspn(string, "\n");
-	// 	string[nl_loc] = '\0';
-	// 	radix_insert_word(root, string);
+	//      nl_loc = strcspn(string, "\n");
+	//      string[nl_loc] = '\0';
+	//      radix_insert_word(root, string);
 	// }
-        // free(string);
+	// free(string);
 	// radix_print(root);
 	fclose(fp);
 
-        // Test for the known words:
-        char *test_words[] = {
-                "places",
-                "pickling",
-                "placebo",
-                "play",
-                "picture",
-                "pickets",
-                "panacea",
-                "pick",
-                "picket",
-                "pickles"
-        };
+	// Test for the known words:
+	char *test_words[] = {
+		"places",
+		"pickling",
+		"placebo",
+		"play",
+		"picture",
+		"pickets",
+		"panacea",
+		"pick",
+		"picket",
+		"pickles"
+	};
 
-        for (int i = 0; i < 10; ++i) {
-                radix_insert_word(root, test_words[i]);
-        }
-        radix_print(root);
+	for (int i = 0; i < 10; ++i) {
+		radix_insert_word(root, test_words[i]);
+	}
+	radix_print(root);
 	// char *test_words[] =
 	//     { "pick", "pla", "play", "banana", "pickling", "p", "placebos",
-        //         "pic", NULL,  };
-        
-        
-        printf("\n\nValid words test:\n");
+	//         "pic", NULL,  };
+
+	printf("\n\nValid words test:\n");
 	for (int i = 0; i < 10; ++i) {
 		if (radix_find_word(root, test_words[i])) {
 			printf("%s:\tFound!\n", test_words[i]);
@@ -60,38 +59,35 @@ int main(void)
 		}
 	}
 
+	// Test for invalid words and prefixes
+	char *invalid_words[] = {
+		"pla",
+		"banana",
+		"p",
+		"placebos",
+		"pic",
+		"pan"
+	};
 
-
-        // Test for invalid words and prefixes
-        char *invalid_words[] = {
-                "pla",
-                "banana",
-                "p",
-                "placebos",
-                "pic",
-                "pan"
-        };
-        
-        // printf("\n\nInvalid words test:\n");
-        // for (int i = 0; i < 6; ++i) {
-	// 	if (radix_find_word(root, invalid_words[i])) {
-	// 		printf("%s:\tFound!\n", invalid_words[i]);
-	// 	} else {
-	// 		printf("%s:\tNot Found\n", invalid_words[i]);
-	// 	}
+	// printf("\n\nInvalid words test:\n");
+	// for (int i = 0; i < 6; ++i) {
+	//      if (radix_find_word(root, invalid_words[i])) {
+	//              printf("%s:\tFound!\n", invalid_words[i]);
+	//      } else {
+	//              printf("%s:\tNot Found\n", invalid_words[i]);
+	//      }
 	// }
 	// if (radix_remove_word(root, "pickling")) {
-        //         printf("pickling deleted\n");
-        // }
+	//         printf("pickling deleted\n");
+	// }
 	// if (!radix_find_word(root, "pickling")) {
-	// 	printf("pickling not found\n");
+	//      printf("pickling not found\n");
 	// }
 
-        radix_find_prefix(root, "p");
-        // radix_find_prefix(root, "picklet");
+	radix_find_prefix(root, "place");
+	// radix_find_prefix(root, "picklet");
 
-
-        radix_delete(&root);
+	radix_delete(&root);
 	return 1;
 
 }
